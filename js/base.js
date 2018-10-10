dc9a20e43aef9ac4b88cab3a0705ee478ab6c6a3
Mrzhzhzh <40493350+Mrzhzhzh@users.noreply.github.com>
2018-10-10 21:59:39 +0800

    commit

--- js/base.js
+++ js/base.js
@@ -1,5 +1,5 @@
 window.base={
-    g_restUrl:'https://solelynet.com/public/index.php/api/v1/',
+    g_restUrl:'http://fuxian.yisuiyanghuoguo.com/api/fuxian/public/index.php/api/v1/',
 
     getData:function(params){
         if(!params.type){
@@ -10,6 +10,7 @@ window.base={
             type:params.type,
             url:this.g_restUrl+params.url,
             data:params.data,
+
             beforeSend: function (XMLHttpRequest) {
                 if (params.tokenFlag) {
                     XMLHttpRequest.setRequestHeader('token', that.getLocalStorage('token'));
@@ -27,7 +28,7 @@ window.base={
     articleList:function(param,callback) {
   
         var allParams = {
-            url:'UserArticle/GetList',
+            url:'Common/Article/get',
             type:'post',
             data:param,
             sCallback: function(data){
@@ -36,6 +37,68 @@ window.base={
         };
         this.getData(allParams)
     },
+
+    upLoadImg:function(param,callback) {
+    
+        $.ajax({ // $.post，告辞
+            type: 'post',
+            contentType: false, // 关关关！必须得 false
+                                // 这个不关会扔一个默认值 application/x-www-form-urlencoded 过去，后端拿不到数据的！
+                                // 而且你甚至不能传个字符串 'multipart/form-data'，后端一样拿不到数据！
+            processData: false, // 关关关！重点
+            url: 'http://fuxian.yisuiyanghuoguo.com/api/fuxian/public/index.php/api/v1/Base/FtpImage/uploadExcluedeToken',
+            data: param,
+            success:function(res){
+                callback && callback(res);
+            },
+            error:function(res){
+                callback && callback(res);
+            }
+        });
+
+    },
+
+
+    labelGet:function(param,callback) {
+  
+        var allParams = {
+            url:'Common/Label/get',
+            type:'post',
+            data:param,
+            sCallback: function(data){
+                callback&&callback(data);
+            }
+        };
+        this.getData(allParams)
+    },   
+
+    messageAdd:function(param,callback) {
+  
+        var allParams = {
+            url:'Common/Message/add',
+            type:'post',
+            data:param,
+            sCallback: function(data){
+                callback&&callback(data);
+            }
+        };
+        this.getData(allParams)
+    },
+
+    articleGet:function(param,callback) {
+  
+        var allParams = {
+            url:'Common/Article/get',
+            type:'post',
+            data:param,
+            sCallback: function(data){
+                callback&&callback(data);
+            }
+        };
+        this.getData(allParams)
+    },
+
+
 
     messages:function(param,callback) {
   
@@ -50,6 +113,18 @@ window.base={
         this.getData(allParams)
     },
 
+    checkComplete:function(obj){
+        var pass = true;
+        for(var key in obj){
+          if(!obj[key]){
+            pass = false;
+          };
+        };
+        return pass;
+        console.log(pass);
+    },
+
+    
 
     articleOne:function(param,callback) {
         var allParams = {
@@ -66,18 +141,6 @@ window.base={
     menuOne:function(param,callback) {
         var allParams = {
             url:'UserMenu/GetInfo',
-            type:'post',
-            data:param,
-            sCallback: function(data){
-                callback&&callback(data);
-            }
-        };
-        this.getData(allParams)
-    },
-
-    menuTree:function(param,callback) {
-        var allParams = {
-            url:'UserMenu/GetTree',
             type:'post',
             data:param,
             sCallback: function(data){
@@ -137,15 +200,54 @@ window.base={
 
     cloneForm:function(form) {
         var res =  JSON.parse(JSON.stringify(form));   
-        return form; 
+        return res; 
     },
     
     getDataSet:function(e) {   
         return e.target.dataset; 
     },
+
+
+    getDataset:function(ele){
+        if(ele.dataset){
+            return ele.dataset;
+        }else{
+            var attrs = ele.attributes,//元素的属性集合
+                dataset = {},
+                name,
+                matchStr;
+
+            for(var i = 0;i<attrs.length;i++){
+                //是否是data- 开头
+                matchStr = attrs[i].name.match(/^data-(.+)/);
+                if(matchStr){
+                    //data-auto-play 转成驼峰写法 autoPlay
+                    name = matchStr[1].replace(/-([\da-z])/gi,function(all,letter){
+                        return letter.toUpperCase();
+                    });
+                    dataset[name] = attrs[i].value;
+                }
+            }
+            return dataset;
+        }
+    },
+
+
+
+    findKeyFromArray:function(Array,key,value) {  
+        var new_array = []; 
+        for (var i = 0; i < Array.length; i++) {
+            
+            if(Array[i][key]&&Array[i][key] == value){
+                new_array.push(Array[i])
+                console.log('Array',Array[i])
+            };
+        };
+        return new_array; 
+    },
     
     GetRequest:function() {  
-       var url = location.search; //获取url中"?"符后的字串  
+       var url = decodeURI(location.search); //获取url中"?"符后的字串  
        var theRequest = new Object();  
        if (url.indexOf("?") != -1) {  
           var str = url.substr(1);  //去掉“？”
@@ -157,10 +259,22 @@ window.base={
        return theRequest;  
     },
 
+    getUrlVars() {
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for (var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+         return vars;
+    }, 
+
 
     computePageArr:function(self) {   
         self.allPages = Math.ceil(self.paginate['count']/self.paginate['pagesize']);
         console.log(self.allPages);
+        console.log(self.paginate);
         self.pageArray = [];
         self.pageArray.push(self.paginate.currentPage);
         if(self.paginate.currentPage+1 <= self.allPages){
